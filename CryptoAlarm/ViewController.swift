e56e7040a22fc2c20b3c5f41c936fe7c98535d62
Bojan Stefanovic <bojanstef4@gmail.com>
2017-12-13 19:34:50 -0800

    Initial Commit

---
+++ CryptoAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoAlarm
+//
+//  Created by Bojan Stefanovic on 12/13/17.
+//  Copyright © 2017 Stefanovic Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
