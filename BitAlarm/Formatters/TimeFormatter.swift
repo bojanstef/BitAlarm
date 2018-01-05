b53034ef0ad3f8c6732cda874ec9994c0dd1dfc5
Bojan Stefanovic <bojanstef4@gmail.com>
2018-01-05 01:43:30 -0800

    Moved CurrencyFormatter to Formatters folder and added TimeFormatter for UNIX time dates in model objects

---
+++ BitAlarm/Formatters/TimeFormatter.swift
@@ -0,0 +1,24 @@
+//
+//  TimeFormatter.swift
+//  BitAlarm
+//
+//  Created by Bojan Stefanovic on 1/3/18.
+//  Copyright © 2018 Stefanovic Ventures. All rights reserved.
+//
+
+import Foundation
+
+final class TimeFormatter: DateFormatter {
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    override init() {
+        super.init()
+        dateStyle = .long
+        timeStyle = .long
+    }
+
+    func date(from timeCreated: TimeInterval) -> Date? {
+        let date = Date(timeIntervalSince1970: timeCreated)
+        let dateString = self.string(from: date)
+        return self.date(from: dateString)
+    }
+}
